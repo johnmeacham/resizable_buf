994b0df67aab5bff9cbd2b5ae8073e59ae854d53
John <john@repetae.net>
2022-06-12 15:33:37 -0700

    move unit tests to their own file

--- resizable_buf.c
+++ resizable_buf.c
@@ -427,34 +427,3 @@ rb_printf(rb_t *rb, char *fmt, ...)
         rb->len += size;
         return size;
 }
-
-// unit tests
-#if 0
-#ifdef NDEBUG
-#undef NDEBUG
-#endif
-
-int main(int argc, char *argv[])
-{
-        rb_t rb = RB_BLANK;
-        assert(!rb_ptr(&rb));
-        assert(!rb_endptr(&rb));
-        assert(!rb_len(&rb));
-        rb_printf(&rb, "hello %i %sX", 43, "test");
-        assert(*RBP(char, &rb) == 'h');
-        assert(*RBPE(char, &rb) == 0);
-        assert(rb_len(&rb) == 14);
-        /* lets pad out with non nulls */
-        rb_grow(&rb, 15);
-        memset(RBPE(char, &rb) + 1, 'x', 14);
-        rb_insert(&rb, 5, ", world", 7);
-        puts(rb_ptr(&rb));
-        assert(!strcmp(rb_ptr(&rb), "hello, world 43 testX"));
-        rb_delete(&rb, 2, 7);
-        puts(rb_ptr(&rb));
-        rb_delete(&rb, 10, 2);
-        puts(rb_ptr(&rb));
-        assert(!strcmp(rb_ptr(&rb), "herld 43 ttX"));
-        return 0;
-}
-#endif
